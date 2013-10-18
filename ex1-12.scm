2e430b317824c00b1720e70df9cb754b949fbac8
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-18 13:39:19 +0530

    -implemented

--- ex1-12.scm
+++ ex1-12.scm
@@ -2,17 +2,26 @@
 	(pascal_counter_call 0 n))
 
 (define (pascal_counter_call row n)
-	(cond (< row n)
-		(pascal_counter row n)
-		(pascal_counter_call (+ row 1) n))
+	(cond ((< row n)
+			(newline)
+			(show-space 0 (- n row))
+			(pascal_counter row row)
+			(pascal_counter_call (+ row 1) n)))
 	)
 
 
 (define (pascal_counter n count)
 	(cond ((> count -1)
 			(display (pascal_recur n count))
+			(display " ")
 			(pascal_counter n (- count 1))) 
 	))
+
+(define (show-space count space-count)
+	(cond ((< count space-count)
+				(display " ")
+				(show-space (+ count 1) space-count)))
+	)
 
 
 (define (pascal_recur row col)
