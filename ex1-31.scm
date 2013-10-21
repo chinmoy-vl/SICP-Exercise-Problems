6d0863b3b6f36a5e1bb5810f6178345af3148e7e
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-21 15:23:20 +0530

    -wallis method of pi calculation in both iterative and recursive method

--- ex1-31.scm
+++ ex1-31.scm
@@ -4,6 +4,13 @@
 		(* (term a)
 			(product term (next a) next b))))
 
+(define (product-iter term a next b)
+	(define (iter a result)
+	(if (> a b)
+		result
+		(iter (next a) (* (term a) result))))
+	(iter a 1))
+
 
 (define (fact x)
 	(define (fact-term x)
@@ -15,7 +22,7 @@
 	(product fact-term 1 next x))
 
 
-(define (wallis-pi n)
+#| (define (wallis-pi n)
 	(define (pi-term n count)
 		(cond ((= n 1)
 			(/ 2.0 3.0))
@@ -23,15 +30,21 @@
 				(/ (* 2 n) (- (* 2 n) 1)))
 			((and (> n 1) (= count 1))
 				(/ (* 2 n) (+ (* 2 n) 1)))))
-
 	(define (next-n n)
 			(+ n 1))
-
 	(define (pi-term-caller n)
 		(cond ((even? n)
 				(pi-term n 0))
 			(else (pi-term n 1))))
-
 	(define (even? n)
 		 (= (remainder n 2) 0))
 	(* 4 (product pi-term-caller 1 next-n n)))
+ |#
+
+
+(define (wallis n)
+	(define (next n)
+		(+ n 1))
+	(define (pi-term n)
+		(/ (* 4.0 n n) (* (- (* 2.0 n) 1) (+ (* 2.0 n) 1))))
+	(* (product-iter pi-term 1 next 100) 2))
