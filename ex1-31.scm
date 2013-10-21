b8eb4987051ef248421ccc45a8afaf1403b9676f
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-21 14:10:05 +0530

    -wip wallis-pi

---
+++ ex1-31.scm
@@ -0,0 +1,37 @@
+(define (product term a next b)
+	(if (> a b)
+		1
+		(* (term a)
+			(product term (next a) next b))))
+
+
+(define (fact x)
+	(define (fact-term x)
+		(if (= x 0)
+			1
+			x))
+	(define (next a)
+		(+ a 1))
+	(product fact-term 1 next x))
+
+
+(define (wallis-pi n)
+	(define (pi-term n count)
+		(cond ((= n 1)
+			(/ 2.0 3.0))
+			((and (> n 1) (= count 0))
+				(/ (* 2 n) (- (* 2 n) 1)))
+			((and (> n 1) (= count 1))
+				(/ (* 2 n) (+ (* 2 n) 1)))))
+
+	(define (next-n n)
+			(+ n 1))
+
+	(define (pi-term-caller n)
+		(cond ((even? n)
+				(pi-term n 0))
+			(else (pi-term n 1))))
+
+	(define (even? n)
+		 (= (remainder n 2) 0))
+	(* 4 (product pi-term-caller 1 next-n n)))
