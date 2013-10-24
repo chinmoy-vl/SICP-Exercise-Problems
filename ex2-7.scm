f9372b21dff1ae2c49ec85718af80faab6f4a41b
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-24 15:29:45 +0530

    -typo

--- ex2-7.scm
+++ ex2-7.scm
@@ -23,7 +23,7 @@
 	(make-interval (min p1 p2 p3 p4)
 									(max p1 p2 p3 p4))))
 
-(define (div-imterval x y)
+(define (div-interval x y)
   (mul-interval x
   							(make-interval (/ 1.0 (upper-bound y))
   															(/ 1.0 (lower-bound y)))))	
