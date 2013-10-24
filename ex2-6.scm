6ebbffc19d35d4da065ab1607f5d85411c8a688f
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-24 14:24:21 +0530

    -WIP church numerals

---
+++ ex2-6.scm
@@ -0,0 +1,17 @@
+(define zero
+  (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+
+;(add-1 zero)
+(define one
+  (lambda (f) (lambda (x) (f x))))
+
+;(add-1 one)
+(define two
+  (lambda (f) (lambda (x) (f (f x)))))
+
+(define (test x)
+  (display (+ x 1)))
