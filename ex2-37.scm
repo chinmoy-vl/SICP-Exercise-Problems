3ca0c3556025b76892b138b7e408e81db7f5332b
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-28 11:22:05 +0530

    -matrix multiplication and transpose

--- ex2-37.scm
+++ ex2-37.scm
@@ -17,12 +17,16 @@
 (define (matrix-*-vector m v)
   (map (lambda (x) (accumulate + 0 (map * v x) )) m))
 
-;(define (transpose mat)
-;  accumulate-n cons () mat)
+(define (transpose mat)
+  (accumulate-n cons (list) mat))
 
-;(define (test-add v w)
-;  (accumulate + 0 (map * v w)))
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+        (map (lambda (row) (matrix-*-vector cols row)) m)))  
 
 
-(define m (list (list 1 2 3) (list 4 5 6) (list 6 7 8)))
+
+
+(define m (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
+(define n (list (list 10 11 12) (list 13 14 15) (list 16 17 18)))
 (define v (list 2 3 4))
