93920152e138dd5f741243c712191263db6b1a4c
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-10-30 14:02:22 +0530

    -code cleanup

--- ex2-61.scm
+++ ex2-61.scm
@@ -1,11 +1,4 @@
 
-(define (equal? a b)
-  (cond ((and (null? b) (null? a)) #t)
-  			((or (null? b) (null? a)) #f)
-        ((and (pair? a) (pair? b)) (and (equal? (car a) (car b))
-        																(equal? (cdr a) (cdr b))))
-        ((or (pair? a) (pair? b)) #f)
-      	(else (eq? a b))))
 
 (define (element-of-set? x set)
   (cond ((null? set) #f)
@@ -21,13 +14,3 @@
                                 (adjoin-set x 
                                             (cdr set))))))
 
-
-(define (intersection-set set1 set2)
-  (cond ((or (null? set1) (null? set2)) '())
-        ((element-of-set? (car set1) set2)
-          (cons (car set1)
-                (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
-
-(define (union-set set1 set2)
-  (append set1 set2))
