1966bdd1486469afd92ef98973a0367edc1c8bc6
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-11-28 14:55:51 +0530

    -changed the indentation

--- stream.scm
+++ stream.scm
@@ -1,5 +1,5 @@
 
-(define (scdr str)	
+(define (scdr str)  
   ((cdr str)))
 
 (define (integers n)
@@ -14,13 +14,13 @@
   (fibs-iterator 0 1))
 
 (define (s-ref s n)
-  	(cond ((= n 0) (car s))
-  	      (else (s-ref (scdr s) (- n 1)))))
+    (cond ((= n 0) (car s))
+          (else (s-ref (scdr s) (- n 1)))))
 
 (define (first-twenty)
   (define (iterate n)
     (cond ((< n 20 ) (display (s-ref (fibs) (+ n 1))) 
-    									(newline) 
-    									(iterate (+ n 1)))
-          (else	(display "done"))))
-	(iterate 0))
+                      (newline) 
+                      (iterate (+ n 1)))
+          (else (display "done"))))
+  (iterate 0))
