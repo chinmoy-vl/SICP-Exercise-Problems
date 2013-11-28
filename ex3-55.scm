439db94541e33084759cd08bb549bfd7b7723aa1
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-11-28 17:40:11 +0530

    -modified to print the first 20 elements of the stream

--- ex3-55.scm
+++ ex3-55.scm
@@ -5,4 +5,19 @@
 
 (define integers (cons-stream 1 (add-streams ones integers)))
 
-(define (partial-sum stream) (cons-stream (car stream) (add-streams (partial-sum stream) (stream-cdr stream))))
+(define (partial-sum stream) (cons-stream (car stream) 
+                                          (add-streams (partial-sum stream) 
+                                                        (stream-cdr stream))))
+
+
+
+(define (generate-stream stream n)
+  (define (generate-stream-iter stream start limit)
+    (cond ((< start limit ) (display (stream-car stream)) 
+                    (newline) 
+                    (generate-stream-iter (stream-cdr stream) (+ start 1) limit))
+        (else (display "done"))))
+  (generate-stream-iter stream 0 n))
+
+(define (first-twenty)
+  (generate-stream (partial-sum integers) 20))
