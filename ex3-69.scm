720ee0d0dcb648249df955771b392043c009b330
chinmoy <chinmoy@vandita-H61M-D2H.(none)>
2013-11-29 15:07:39 +0530

    -generating Pythagorean triples

--- ex3-69.scm
+++ ex3-69.scm
@@ -1,36 +1,64 @@
+(define ones
+  (cons-stream 1 ones))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define integers 
+  (cons-stream 1 
+                (add-streams ones integers)))
+
+
 (define (interleave s1 s2)
   (if (stream-null? s1)
       s2
       (cons-stream (stream-car s1)
-      							(interleave s2 (stream-cdr s1)))))
+                    (interleave s2 (stream-cdr s1)))))
 
 (define (pairs s t)
   (cons-stream
-   (list (stream-car s) (stream-car t))
-   (interleave-streams
-    (stream-map (lambda (x) (list (stream-car s) x))
-                (stream-cdr t))
-    (stream-map (lambda (x) (list x (stream-car t)))
-                (stream-cdr s))
-    (pairs (stream-cdr s) (stream-cdr t)))))
+    (list (stream-car s) (stream-car t))
+    (interleave 
+      (stream-map (lambda (x) (list (stream-car s) x))
+                t)
+      (pairs (stream-cdr s) (stream-cdr t)))))
 
-(define (integers-from-n n)
-  (cons-stream n (integers-from-n (+ 1 n))))
-
-(define integers (integers-from-n 1))
 
 (define (triples s t u)
-  (cons-stream 
+  (cons-stream
    (list (stream-car s) (stream-car t) (stream-car u))
    (interleave
-    (stream-map (lambda (x) (cons (stream-car s)  x))
-                (pairs t (stream-cdr u)))
+    (stream-map (lambda (x) (cons (stream-car s) x))
+                (pairs (stream-cdr t) (stream-cdr u)))
     (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
 
-(define triplets (triples integers integers integers))
 
-(define (pyhta-theorem-filter l)
-  (= (+ (square (car l)) (square (cadr l)))
-     (square (caddr l))))
 
-(define Pytha-triplets (stream-filter pyhta-theorem-filter triplets))
+;(define (triples s t u)
+;  (cons-stream
+;   (list (stream-car s) (stream-car t) (stream-car u))
+;   (interleave
+;    (stream-map (lambda (x) (list (stream-car s) (stream-car (stream-cdr t)) x))
+;                (stream-cdr u))
+;    (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
+
+
+(define (generate-stream stream n)
+  (define (generate-stream-iter stream start limit)
+    (cond ((< start limit ) (display (stream-car stream)) 
+                    (newline) 
+                    (generate-stream-iter (stream-cdr stream) (+ start 1) limit))
+        (else (display "done"))))
+  (generate-stream-iter stream 0 n))
+
+
+(define (pyhtagorean triplet)
+  (= (+ (square (car triplet))
+        (square (cadr triplet)))
+      (square (caddr triplet)))) 
+
+(define (first-n n)
+  (generate-stream (stream-filter pyhtagorean (triples integers integers integers) ) n))
+
+
+
